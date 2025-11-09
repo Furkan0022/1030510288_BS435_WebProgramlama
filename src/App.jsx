b4d6514aa0ab80c28f93707c3cb61f5ec2da7bd4
Furkan <1030510288@erciyes.edu.tr>
2025-11-09 13:37:08 +0300

    Kurulu gerçekleşti

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+// import './App.css' // CSS dosyasını bağlanacak
+
+function App() {
+  return (
+    <div className="game-container">
+      <h1>AI mi Gerçek mi Oyununa Hoş Geldiniz!</h1>
+      <p>Bu alanda başlangiç kurallari ve "Başla" butonu görünecek.</p>
+    </div>
+  )
+}
+
+export default App
